1b1cfe428b66ef9e4ef27a35b134e101f377d655
TimmyTmai <160735110+TimmyTmai@users.noreply.github.com>
2025-11-11 16:06:24 +1100

    Define basic Animal class

--- animal.py
+++ animal.py
@@ -6,3 +6,22 @@ ID: 110100110
 Username: bizvy001
 This is my own work as defined by the University's Academic Integrity Policy.
 '''
+from abc import ABC, abstractmethod
+
+class Animal(ABC):
+    def __init__(self, name,species, age, diet):
+        self.__name = name
+        self.__species = species
+        self.__age = age
+        self.__diet = diet
+
+    def eat(self):
+        return f"{self.__name} is eating {self.__diet}."
+
+    def sleep(self):
+        return f"{self.__name} curls up and sleeps."
+
+    @abstractmethod
+    def make_sound(self):
+        pass
+
